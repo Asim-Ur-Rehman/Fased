f7cafcfa230921aee036de8e66dce11dd2a882a5
Asim-Ur-Rehman <asim.rehman@techstirr.com>
2021-11-11 14:50:24 +0500

    AuthHeader Done

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -3,31 +3,32 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import { Images } from '../../constants/images'
 import { theme } from '../../constants/theme'
 import Button from '../../components/Button'
+import { AuthHeader } from '../../components/AuthHeader/AuthHeader'
 
 export const SignIn = ({ navigation }) => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.backgrounds.whiteBG
-      }}>
-      <Text> SignIn Screen </Text>
+    return (
+        <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: theme.backgrounds.whiteBG
+            }}>
+            <Text> SignIn Screen </Text>
 
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('SignUp')
-        }}>
-        <Text>Next</Text>
-      </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    navigation.navigate('SignUp')
+                }}>
+                <Text>Next</Text>
+            </TouchableOpacity>
 
-      <Button
-        linearColor1="#FE0000"
-        linearColor2="#680000"
-        startColor={{ x: 0, y: 1 }}
-        endColor={{ x: 1, y: 0 }}
-      />
-    </View>
-  )
+            <Button
+                linearColor1="#FE0000"
+                linearColor2="#680000"
+                startColor={{ x: 0, y: 1 }}
+                endColor={{ x: 1, y: 0 }}
+            />
+        </View>
+    )
 }
