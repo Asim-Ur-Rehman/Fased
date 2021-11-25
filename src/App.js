7199794e06be3a9ecc21c9d34285517e5649e4c4
Asifali2002 <asif.bholim90@gmail.com>
2021-11-25 21:24:18 +0500

    Add NEws SCreeen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import 'react-native-gesture-handler'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import MainNavigation from './navigation/navigation'
 import { Provider } from 'react-redux'
 import { store } from './stores'
 import SplashScreen from 'react-native-splash-screen'
 import { View } from 'react-native'
-import { Calendar } from 'react-native-calendars';
+import { Calendar } from 'react-native-calendars'
 import { CustomScrollBarComponent } from './components/ScrollBarComponent/ScollBarComp'
-import 'react-native-gesture-handler';
+import 'react-native-gesture-handler'
+import CategoryComp from './components/CategoryComponent/index'
+import { Images } from './constants/images'
+import Radio from './components/CategoryComponent/radio'
+import { ScrollView } from 'react-native-gesture-handler'
+import { NewsDetails } from './screens/News/NewsDetails'
+
 const App = () => {
   useEffect(() => {
     setTimeout(
@@ -19,9 +25,71 @@ const App = () => {
   }, [])
 
 
+
+  // const Data = [
+  //   {
+  //     key: 1,
+  //     title: 'Killing',
+  //     description:
+  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel',
+  //     Image: Images.Pictures.category,
+  //     backgroundColor: '#DF0707'
+  //   },
+  //   {
+  //     key: 2,
+  //     title: 'Harrasment',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel in sapien ultrices id quam nam.',
+  //     Image: Images.Pictures.harasment,
+  //     backgroundColor: '#FFA724'
+  //   },
+  //   {
+  //     key: 3,
+  //     title: 'Kidnapping',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel in sapien ultrices id quam nam.',
+  //     Image: Images.Pictures.kidnap,
+  //     backgroundColor: '#CF00BA'
+  //   },
+  //   {
+  //     key: 4,
+  //     title: 'Robery',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel in sapien ultrices id quam nam.',
+  //     Image: Images.Pictures.robery,
+  //     backgroundColor: '#5819C1'
+  //   },
+  //   {
+  //     key: 5,
+  //     title: 'Snatching',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel in sapien ultrices id quam nam.',
+  //     Image: Images.Pictures.snatch,
+  //     backgroundColor: '#211DE8'
+  //   },
+  //   {
+  //     key: 6,
+  //     title: 'Assault',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, sit justo vel in sapien ultrices id quam nam.',
+  //     Image: Images.Pictures.assault,
+  //     backgroundColor: '#0CB9A2'
+  //   },
+
+  // ];
+
   return (
     <>
+      {/* <ScrollView contentContainerStyle={{flexGrow:1, justifyContent:"center", alignItems:"center"}} showsVerticalScrollIndicator={false}
+      style={{flex:1}}
+        >
+        <CategoryComp data={Data} targetKey={"key"} />
+      </ScrollView> */}
+
+   
+
       <Provider store={store}>
+        {/* <NewsDetails/> */}
       <View style={{ flex: 1, backgroundColor: '#fff' }}>
         <MainNavigation />
       </View>
@@ -31,3 +99,22 @@ const App = () => {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/CategoryComponent/index.js
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import { Icon } from 'react-native-vector-icons/Ionicons'
+import { Images } from '../../constants/images'
+
+export default function CategoryComp({ 
+  onPress = () => {}, 
+  data = [],
+  targetKey = "1" 
+}) {
+
+ 
+  const [click, setClick] = useState(true)
+  const [selected, setSelected] = useState([])
+
+  const onSelect = (item) => {
+    var arr = []
+    const isSelected =  selected.findIndex((e) => e[targetKey] == item[targetKey] )
+    if (isSelected == -1) {
+      arr.push(item)
+      setSelected([...selected, ...arr])
+      setClick(!click)
+    }else {
+      var newArr = selected
+      newArr.splice(isSelected, 1)
+      setClick(!click)
+      setSelected(newArr)
+    }
+  }
+
+  // console.log("selected", selected)
+
+  return (
+    <>            
+    <Text style={{height:30,fontSize: 17,marginTop:15,fontFamily:'Rubik-Medium'}}> 
+      Selected Categories {data.length - selected.length}
+    </Text>
+      {data.map((item, i) => {
+        const isInclude =  selected.findIndex((e) => e[targetKey] == item[targetKey])
+        const isSelected = isInclude == -1 ? true : false
+        // console.log('isSelected', isSelected)
+        return (
+          <>
+        
+            <TouchableOpacity
+              onPress={() => onSelect(item)}
+              key={i}
+              style={[
+                styles.mainContainer,
+                {
+                  backgroundColor: isSelected
+                    ? item.backgroundColor
+                    : `${item.backgroundColor}18`,
+                  borderColor: item.backgroundColor
+                }
+              ]}
+              activeOpacity={1}>
+              <View style={styles.ImageView}>
+                <Image source={item.Image} />
+              </View>
+              <View style={{ flex: 1, marginLeft: 15 }}>
+                <View style={{ alignItems: 'flex-end', marginTop: 4 }}>
+                  <View
+                    style={{
+                      height: 12,
+                      width: 12,
+                      borderRadius: 6,
+                      borderWidth: 1.4,
+                      borderColor: isSelected ? '#fff' : item.backgroundColor,
+                      alignItems: 'center',
+                      justifyContent: 'center'
+                    }}>
+                    <View
+                      style={{
+                        height: 5.5,
+                        width: 5.5,
+                        borderRadius: 3,
+                        backgroundColor: isSelected ? '#fff' : item.backgroundColor
+                      }}
+                    />
+                  </View>
+                </View>
+                <View style={styles.TextView}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontFamily: 'Rubik-Medium',
+                      lineHeight: 17,
+                      letterSpacing: 0.08,
+                      marginBottom: 3,
+                      color: isSelected ? '#fff' : item.backgroundColor
+                    }}>
+                    {item.title}
+                  </Text>
+                  <Text
+                    style={{
+                      fontSize: 11,
+                      fontFamily: 'Rubik-Thin',
+                      lineHeight: 15,
+                      letterSpacing: 0.08,
+                      color: isSelected ? '#fff' : '#000'
+                    }}>
+                    {item.description}
+                  </Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </>
+        )
+      })}
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: 100,
+    width: '90%',
+    flexDirection: 'row',
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 5,
+    borderWidth: 1.4
+  },
+  ImageView: {
+    height: 80,
+    width: 78,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ffff',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+
+    elevation: 8
+  },
+  TextView: {
+    height: 75,
+  }
+})
