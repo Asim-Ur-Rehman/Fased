041f58ea038d1739e43a96deef67b715276742c4
Asim-Ur-Rehman <asim.rehman@techstirr.com>
2021-12-14 18:31:42 +0500

    'taosadded'

--- src/components/ToastMessage/ToastMessage.js
+++ src/components/ToastMessage/ToastMessage.js
@@ -2,21 +2,17 @@ import React from 'react';
 import Toast from 'react-native-toast-message';
 
 const ToastMessage = (text1, text2, type, position) => {
-    // Toast.show({
-    //     type: type,
-    //     position: position ? position : 'top',
-    //     text1: text1,
-    //     text2: text2,
-    //     visibilityTime: 4000,
-    //     autoHide: true,
-    //     topOffset: 30,
-    //     bottomOffset: 40,
-
-    // });
     Toast.show({
-        type: 'success',
-        text1: 'Hello',
-        text2: 'This is some something 👋'
+        type: type,
+        position: position ? position : 'top',
+        text1: text1,
+        text2: text2,
+        visibilityTime: 4000,
+        autoHide: true,
+        topOffset: 30,
+        bottomOffset: 40,
+
     });
+
 }
 export default ToastMessage;
